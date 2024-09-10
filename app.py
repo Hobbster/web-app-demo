4fcd75574d9739121ffae2c9bd6986c6d6457dd9
Hobbster <dave@hobbs-family.com>
2024-09-10 17:43:00 +0100

    Update application to include debug and home() function

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return '<h1>Welcome to Web-App !</h1>'
 
-@app.route('/about/')
+@app.route('/about')
 def about():
     return '<h3>This is the About Page of the Web-App</h3>'
 
